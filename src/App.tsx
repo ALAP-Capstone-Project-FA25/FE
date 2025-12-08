9f9125dc747e618d40fbc2489f5b932f11d1539d
Tran Minh Hieu <hieutm2001@gmail.com>
2025-12-08 22:28:34 +0700

    setting

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Toaster } from './components/ui/toaster';
+import AppProvider from './providers';
+import AppRouter from './routes';
+
+export default function App() {
+  return (
+    <AppProvider>
+      <AppRouter />
+
+      <Toaster />
+    </AppProvider>
+  );
+}
